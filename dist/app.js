5ebd1c596802bd72e16d9087caf2d441b9de307c
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-17 23:13:59 +0200

    Changed file structure v2

---
+++ dist/app.js
@@ -0,0 +1,3 @@
+"use strict";
+const consoleLog = "color: pink; background-color: black; font-weight: bold";
+console.log("%c TypeScript ConsoleLog", consoleLog);
--- scripts/app.js
+++
@@ -1,2 +0,0 @@
-var consoleLog = "color: pink; background-color: black; font-weight: bold";
-console.log("%c TypeScript ConsoleLog", consoleLog);
