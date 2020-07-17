592fa2f73ab3ae7723087c67f3549933ebd4b3bf
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-17 21:17:12 +0200

    Additional setup

---
+++ app.js
@@ -0,0 +1,2 @@
+var consoleLog = "color: pink; background-color: black; font-weight: bold";
+console.log("%c TypeScript ConsoleLog", consoleLog);
---
+++ app.ts
@@ -0,0 +1,2 @@
+const consoleLog = "color: pink; background-color: black; font-weight: bold";
+console.log("%c TypeScript ConsoleLog", consoleLog);
