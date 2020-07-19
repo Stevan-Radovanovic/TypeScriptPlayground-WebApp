bbd9bf1771c25c9176b27fe924df8b65729730f9
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-19 22:45:57 +0200

    Added extensive validation function

--- dist/project-manager/app.js
+++ dist/project-manager/app.js
@@ -7,21 +7,77 @@ class ProjectInput {
         this.content = document.getElementById("project-input");
         this.renderContentHere = document.getElementById("app");
         const importedContent = document.importNode(this.content.content, true);
-        this.renderContentHere.id = "user-input";
+        const element = importedContent.firstElementChild;
+        element.id = "user-input";
         console.log("%c Binding Form Properties", consoleLogString);
-        this.projectTitle = this.renderContentHere.querySelector("#title");
-        this.projectDescription = this.renderContentHere.querySelector("#description");
-        this.projectPeople = this.renderContentHere.querySelector("#people");
+        this.projectTitle = element.querySelector("#title");
+        this.projectDescription = element.querySelector("#description");
+        this.projectPeople = element.querySelector("#people");
         this.renderContentHere.addEventListener("submit", this.submitHandler.bind(this));
-        this.renderContentHere.insertAdjacentElement("afterbegin", importedContent.firstElementChild);
+        this.renderContentHere.insertAdjacentElement("afterbegin", element);
     }
     submitHandler() {
         console.log("%c Validating form", consoleLogString);
         event === null || event === void 0 ? void 0 : event.preventDefault();
-        this.gatherInput();
+        const userInput = this.gatherInput();
+        if (userInput) {
+            alert("Project added");
+            this.clearInputs();
+        }
+        else {
+            alert("It ain't right");
+        }
+    }
+    clearInputs() {
+        console.log("%c Reseting form", consoleLogString);
+        this.projectDescription.value = "";
+        this.projectTitle.value = "";
+        this.projectPeople.value = "";
     }
     gatherInput() {
+        var _a, _b, _c;
         console.log("%c Gathering Input", consoleLogString);
+        const title = (_a = this.projectTitle) === null || _a === void 0 ? void 0 : _a.value;
+        const desc = (_b = this.projectDescription) === null || _b === void 0 ? void 0 : _b.value;
+        const people = +((_c = this.projectPeople) === null || _c === void 0 ? void 0 : _c.value);
+        if (!Helper.validatePrimitive(title, {}) ||
+            !Helper.validatePrimitive(desc, {}) ||
+            !Helper.validatePrimitive(people, { min: 0 }))
+            return;
+        return [title, desc, people];
+    }
+}
+class Helper {
+    static validatePrimitive(value, configuration) {
+        if (typeof value === "string") {
+            if (value === null || value === undefined || value.trim().length === 0)
+                return false;
+            if (configuration.contains && !value.includes(configuration.contains)) {
+                return false;
+            }
+            if (configuration.minLength &&
+                value.trim().length < configuration.minLength) {
+                return false;
+            }
+            if (configuration.maxLength &&
+                value.trim().length > configuration.maxLength) {
+                return false;
+            }
+        }
+        if (typeof value === "number") {
+            console.log("Here");
+            console.log(configuration.min);
+            if (value === null || value === undefined || value === 0)
+                return false;
+            if (configuration.max !== undefined && value > configuration.max) {
+                return false;
+            }
+            if (configuration.min !== undefined && value < configuration.min) {
+                return false;
+            }
+            console.log("Here 2");
+        }
+        return true;
     }
 }
 const projectInput = new ProjectInput();
--- src/project-manager/app.ts
+++ src/project-manager/app.ts
@@ -18,38 +18,112 @@ class ProjectInput {
     this.renderContentHere = document.getElementById("app")! as HTMLDivElement;
 
     const importedContent = document.importNode(this.content.content, true);
-    this.renderContentHere.id = "user-input";
+    const element = importedContent.firstElementChild as HTMLFormElement;
+    element.id = "user-input";
 
     console.log("%c Binding Form Properties", consoleLogString);
-    this.projectTitle = this.renderContentHere.querySelector(
-      "#title"
-    )! as HTMLInputElement;
-    this.projectDescription = this.renderContentHere.querySelector(
+    this.projectTitle = element.querySelector("#title")! as HTMLInputElement;
+    this.projectDescription = element.querySelector(
       "#description"
     )! as HTMLInputElement;
-    this.projectPeople = this.renderContentHere.querySelector(
-      "#people"
-    )! as HTMLInputElement;
+    this.projectPeople = element.querySelector("#people")! as HTMLInputElement;
 
     this.renderContentHere.addEventListener(
       "submit",
       this.submitHandler.bind(this)
     );
 
-    this.renderContentHere.insertAdjacentElement(
-      "afterbegin",
-      importedContent.firstElementChild!
-    );
+    this.renderContentHere.insertAdjacentElement("afterbegin", element);
   }
 
   submitHandler() {
     console.log("%c Validating form", consoleLogString);
     event?.preventDefault();
-    this.gatherInput();
+    const userInput = this.gatherInput();
+    if (userInput) {
+      alert("Project added");
+      this.clearInputs();
+    } else {
+      alert("It ain't right");
+    }
   }
 
-  gatherInput() {
+  clearInputs() {
+    console.log("%c Reseting form", consoleLogString);
+    this.projectDescription.value = "";
+    this.projectTitle.value = "";
+    this.projectPeople.value = "";
+  }
+
+  gatherInput(): [string, string, number] | void {
     console.log("%c Gathering Input", consoleLogString);
+    const title = this.projectTitle?.value;
+    const desc = this.projectDescription?.value;
+    const people = +this.projectPeople?.value;
+    if (
+      !Helper.validatePrimitive(title, {}) ||
+      !Helper.validatePrimitive(desc, {}) ||
+      !Helper.validatePrimitive(people, { min: 0 })
+    )
+      return;
+    return [title, desc, people];
+  }
+}
+
+//Validation object Interface
+interface ValidationConfiguration {
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+  contains?: string;
+}
+
+//Helper Class
+class Helper {
+  static validatePrimitive(
+    value: string | number,
+    configuration: ValidationConfiguration
+  ): boolean {
+    if (typeof value === "string") {
+      if (value === null || value === undefined || value.trim().length === 0)
+        return false;
+
+      if (configuration.contains && !value.includes(configuration.contains)) {
+        return false;
+      }
+
+      if (
+        configuration.minLength &&
+        value.trim().length < configuration.minLength
+      ) {
+        return false;
+      }
+
+      if (
+        configuration.maxLength &&
+        value.trim().length > configuration.maxLength
+      ) {
+        return false;
+      }
+    }
+
+    if (typeof value === "number") {
+      console.log("Here");
+      console.log(configuration.min);
+      if (value === null || value === undefined || value === 0) return false;
+
+      if (configuration.max !== undefined && value > configuration.max) {
+        return false;
+      }
+
+      if (configuration.min !== undefined && value < configuration.min) {
+        return false;
+      }
+      console.log("Here 2");
+    }
+
+    return true;
   }
 }
 
