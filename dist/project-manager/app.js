44eed2a4ac2b9522aa530094c51b6218075e7705
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-19 20:36:16 +0200

    Started ProjectInput class

--- dist/project-manager/app.js
+++ dist/project-manager/app.js
@@ -1,3 +1,27 @@
 "use strict";
 const consoleLogString = "color: pink; background-color: black; font-weight: bold";
 console.log("%c Project Manager", consoleLogString);
+class ProjectInput {
+    constructor() {
+        console.log("%c Initializing Project Form", consoleLogString);
+        this.content = document.getElementById("project-input");
+        this.renderContentHere = document.getElementById("app");
+        const importedContent = document.importNode(this.content.content, true);
+        this.renderContentHere.id = "user-input";
+        console.log("%c Binding Form Properties", consoleLogString);
+        this.projectTitle = this.renderContentHere.querySelector("#title");
+        this.projectDescription = this.renderContentHere.querySelector("#description");
+        this.projectPeople = this.renderContentHere.querySelector("#people");
+        this.renderContentHere.addEventListener("submit", this.submitHandler.bind(this));
+        this.renderContentHere.insertAdjacentElement("afterbegin", importedContent.firstElementChild);
+    }
+    submitHandler() {
+        console.log("%c Validating form", consoleLogString);
+        event === null || event === void 0 ? void 0 : event.preventDefault();
+        this.gatherInput();
+    }
+    gatherInput() {
+        console.log("%c Gathering Input", consoleLogString);
+    }
+}
+const projectInput = new ProjectInput();
--- src/project-manager/app.ts
+++ src/project-manager/app.ts
@@ -1,3 +1,56 @@
 const consoleLogString =
   "color: pink; background-color: black; font-weight: bold";
 console.log("%c Project Manager", consoleLogString);
+
+//Project Input Class
+class ProjectInput {
+  content: HTMLTemplateElement;
+  renderContentHere: HTMLDivElement;
+  projectTitle: HTMLInputElement;
+  projectDescription: HTMLInputElement;
+  projectPeople: HTMLInputElement;
+
+  constructor() {
+    console.log("%c Initializing Project Form", consoleLogString);
+    this.content = document.getElementById(
+      "project-input"
+    )! as HTMLTemplateElement;
+    this.renderContentHere = document.getElementById("app")! as HTMLDivElement;
+
+    const importedContent = document.importNode(this.content.content, true);
+    this.renderContentHere.id = "user-input";
+
+    console.log("%c Binding Form Properties", consoleLogString);
+    this.projectTitle = this.renderContentHere.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.projectDescription = this.renderContentHere.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.projectPeople = this.renderContentHere.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.renderContentHere.addEventListener(
+      "submit",
+      this.submitHandler.bind(this)
+    );
+
+    this.renderContentHere.insertAdjacentElement(
+      "afterbegin",
+      importedContent.firstElementChild!
+    );
+  }
+
+  submitHandler() {
+    console.log("%c Validating form", consoleLogString);
+    event?.preventDefault();
+    this.gatherInput();
+  }
+
+  gatherInput() {
+    console.log("%c Gathering Input", consoleLogString);
+  }
+}
+
+const projectInput = new ProjectInput();
