c8e4a439b9fe6a11e5a3a9bc12fbc94964763d79
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-22 19:12:15 +0200

    Added dummy database and player model

---
+++ dist/rest-api/database/db.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database = [
+    {
+        id: Math.random().toString(),
+        jerseyNumber: 7,
+        name: "Raheem Sterling",
+        position: "WF",
+    },
+    {
+        id: Math.random().toString(),
+        jerseyNumber: 42,
+        name: "Yaya Toure",
+        position: "CM",
+    },
+    {
+        id: Math.random().toString(),
+        jerseyNumber: 21,
+        name: "David Silva",
+        position: "CAM",
+    },
+    {
+        id: Math.random().toString(),
+        jerseyNumber: 47,
+        name: "Phil Foden",
+        position: "AM",
+    },
+];
---
+++ src/rest-api/database/db.ts
@@ -0,0 +1,28 @@
+import Player from "../models/player";
+
+const database: Player[] = [
+  {
+    id: Math.random().toString(),
+    jerseyNumber: 7,
+    name: "Raheem Sterling",
+    position: "WF",
+  },
+  {
+    id: Math.random().toString(),
+    jerseyNumber: 42,
+    name: "Yaya Toure",
+    position: "CM",
+  },
+  {
+    id: Math.random().toString(),
+    jerseyNumber: 21,
+    name: "David Silva",
+    position: "CAM",
+  },
+  {
+    id: Math.random().toString(),
+    jerseyNumber: 47,
+    name: "Phil Foden",
+    position: "AM",
+  },
+];
