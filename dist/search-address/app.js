1ce0c926178e8daa66557454887c4ef97a3585c4
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-07-22 15:01:48 +0200

    Added incomplete version 1.0 of SearchAdress - Map not shown correctly

--- dist/search-address/app.js
+++ dist/search-address/app.js
@@ -4,8 +4,23 @@ console.log("%c Search Address", consoleStyle);
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const div = document.getElementById("map");
-function getUserInput() {
+button.addEventListener("click", searchAddressHandler);
+function searchAddressHandler(event) {
+    event.preventDefault();
     const value = input.value;
     console.log("%c Fetching input: " + value, consoleStyle);
+    const coordinates = { lat: 40.41, lng: -73.99 };
+    new ol.Map({
+        target: "map",
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM(),
+            }),
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([coordinates.lat, coordinates.lng]),
+            zoom: 16,
+        }),
+    });
+    const view = document.getElementsByClassName("ol-viewport")[0];
 }
-button.addEventListener("click", getUserInput);
--- src/search-address/app.ts
+++ src/search-address/app.ts
@@ -1,13 +1,36 @@
 const consoleStyle = "background-color: black; font-weight: bold; color:white";
 console.log("%c Search Address", consoleStyle);
 
+declare var ol: any;
+
 const button = document.getElementById("button")! as HTMLButtonElement;
 const input = document.getElementById("input")! as HTMLInputElement;
 const div = document.getElementById("map")! as HTMLDivElement;
 
-function getUserInput() {
+button.addEventListener("click", searchAddressHandler);
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
   const value = input.value;
   console.log("%c Fetching input: " + value, consoleStyle);
-}
+  //Using Dummy variables
+  const coordinates = { lat: 40.41, lng: -73.99 };
 
-button.addEventListener("click", getUserInput);
+  //div.innerHTML = "";
+  new ol.Map({
+    target: "map",
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM(),
+      }),
+    ],
+    view: new ol.View({
+      center: ol.proj.fromLonLat([coordinates.lat, coordinates.lng]),
+      zoom: 16,
+    }),
+  });
+
+  const view = document.getElementsByClassName(
+    "ol-viewport"
+  )[0] as HTMLDivElement;
+}
